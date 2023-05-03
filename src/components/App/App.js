362599f6dfd4c14453c1256f2ce1d250dcc349d1
gregor <gregor.aubrey.uk@gmail.com>
2023-05-03 17:08:15 +0100

    created react app and deleted logo image file that comes by default

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Todo List</h1>
+      <footer>Made by Gregor Aubrey</footer>
+    </div>
+  );
+}
+
+export default App;
